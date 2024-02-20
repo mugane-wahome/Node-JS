fa2f69b7dc31466cb9ecd50d7fe7efa54b229b2d
mugane-wahome <muganewahome@gmail.com>
2024-02-20 16:58:35 +0300

    Creating files using node

---
+++ 3. creating file using node/index.js
@@ -0,0 +1,20 @@
+
+// var fs = require("fs");
+// var createStream = fs.createWriteStream("files.txt");
+// createStream.end();
+
+
+const fsPromise = require('fs').promises
+const path = require('path')
+
+const createFile = async ()=>{
+    try{
+        const data = await fsPromise.appendFile(path.join(__dirname, 'filess.txt'), 'Hello World\n')
+        console.log('File created successfully')
+    }
+
+    catch(err){
+        console.log(err)
+    }
+}
+createFile()
